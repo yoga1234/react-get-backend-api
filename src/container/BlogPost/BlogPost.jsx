a386c0193e0c0659948692075b3a6b2c7193ddc3
Septiana Yoga <septianayoga30@gmail.com>
2020-05-15 15:53:51 +0700

    add post req

--- src/container/BlogPost/BlogPost.jsx
+++ src/container/BlogPost/BlogPost.jsx
@@ -5,7 +5,13 @@ import axios from 'axios'
 
 class BlogPost extends Component {
   state = {
-    post: []
+    post: [],
+    formBlogPost: {
+      userId: 1,
+      id: 1,
+      title: '',
+      body: ''
+    }
   }
   componentDidMount() {
     this.getPostAPI()
@@ -26,10 +32,26 @@ class BlogPost extends Component {
     })
   }
 
+  handleFormChange = (e) => {
+    let formBlogPostNew = {...this.state.formBlogPost}
+    formBlogPostNew[e.target.name] = e.target.value
+    this.setState({
+      formBlogPost: formBlogPostNew
+    }, () => {
+      console.log('formBlogPost', this.state.formBlogPost)
+    })
+  }
+
   render() {
     return (
       <Fragment>
-        <p className="blog-post-title">Blog Post</p>
+        <div className="form-add-post">
+          <label htmlFor="title">Title</label>
+          <input type="text" name="title" placeholder="Add title here" onChange={this.handleFormChange}/>
+          <label htmlFor="body">Blog Content</label>
+          <textarea name="body" id="body" cols="30" rows="10" onChange={this.handleFormChange}></textarea>
+          <button className="btn-submit">Simpan</button>
+        </div>
         {
           this.state.post.map(post => {
             return <Post key={post.id} title={post.title} data={post} desc={post.body} remove={this.handleRemove}/>
