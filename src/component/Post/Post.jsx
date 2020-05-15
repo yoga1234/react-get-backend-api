7845655dac482333b1b124b441a81c3b44b33da5
Septiana Yoga <septianayoga30@gmail.com>
2020-05-15 13:55:35 +0700

    add remove button

--- src/component/Post/Post.jsx
+++ src/component/Post/Post.jsx
@@ -7,8 +7,9 @@ const Post = (props) => {
         <img src="http://placeimg.com/200/150/tech" alt="post thumbnail"/>
       </div>
       <div className="content">
-        <p className="title">{props.title}</p>
-        <p className="desc">{props.desc}</p>
+        <p className="title">{props.data.title}</p>
+        <p className="desc">{props.data.body}</p>
+        <button className="remove" onClick={() => props.remove(props.data.id)}>Remove</button>
       </div>
     </div>
   )
--- src/container/BlogPost/BlogPost.jsx
+++ src/container/BlogPost/BlogPost.jsx
@@ -1,27 +1,38 @@
 import React, { Component, Fragment } from 'react'
 import './BlogPost.css'
 import Post from '../../component/Post/Post'
+import axios from 'axios'
 
 class BlogPost extends Component {
   state = {
     post: []
   }
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then(response => response.json())
-      .then(json => {
-        this.setState({
-          post: json
-        })
-      })
+    this.getPostAPI()
   }
+
+  getPostAPI = () => {
+    axios.get('http://localhost:3004/posts')
+    .then((result) => {
+      this.setState({
+        post: result.data
+      })
+    })
+  }
+
+  handleRemove = (data) => {
+    axios.delete(`http://localhost:3004/posts/${data}`).then((res) => {
+      this.getPostAPI()
+    })
+  }
+
   render() {
     return (
       <Fragment>
         <p className="blog-post-title">Blog Post</p>
         {
           this.state.post.map(post => {
-            return <Post key={post.id} title={post.title} desc={post.body}/>
+            return <Post key={post.id} title={post.title} data={post} desc={post.body} remove={this.handleRemove}/>
           })
         }
       </Fragment>
